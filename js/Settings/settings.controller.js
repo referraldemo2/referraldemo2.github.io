c112baa8069b633663a3a3fae8d36a5fe3c23b3a
mayank <mayank.mishra@naukri.com>
2016-03-03 17:57:15 +0530

    logo updated

--- js/Settings/settings.controller.js
+++ js/Settings/settings.controller.js
@@ -1,7 +1,7 @@
 referralApp.factory('settingsFactory',['$http','__url',function($http,__url){
     
     var settingsFactory = {};
-    var cache = {"data":{"logo":"https:\/\/st.referralrecruit.com\/ReferralImages\/Logos\/691203.gif?1439816793","program":"Mycompany Referral / IJP Program"}};
+    var cache = {"data":{"logo":"https:\/\/referraldemo2.github.io\/img\/logo.png","program":"Mycompany Referral / IJP Program"}};
     
     settingsFactory.getSettings = function(__callback){
         return cache ? __callback(cache) : $http.get(__url+'/settings').success(__callback);
