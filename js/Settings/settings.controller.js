be408dd56946ccb13116692f884a097475480dd6
mayank <mayank.mishra@naukri.com>
2016-03-03 18:02:53 +0530

    updated logo with smaller size .

--- js/Settings/settings.controller.js
+++ js/Settings/settings.controller.js
@@ -1,7 +1,7 @@
 referralApp.factory('settingsFactory',['$http','__url',function($http,__url){
     
     var settingsFactory = {};
-    var cache = {"data":{"logo":"https:\/\/referraldemo2.github.io\/img\/logo.png","program":"Mycompany Referral / IJP Program"}};
+    var cache = {"data":{"logo":"https:\/\/referraldemo2.github.io\/img\/logo.jpg","program":"Mycompany Referral / IJP Program"}};
     
     settingsFactory.getSettings = function(__callback){
         return cache ? __callback(cache) : $http.get(__url+'/settings').success(__callback);
